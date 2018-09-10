484532bcb8f98b643da1a279efe74528803d9036
keram <mlabos@bookingbug.com>
2018-09-10 11:00:27 +0100

    Ad safe local var value

--- system-custom.el
+++ system-custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-command "/usr/bin/pandoc")
- '(package-selected-packages
-   '(which-key go-mode rbenv org-present org-plus-contrib web-mode js-doc js2-mode json-mode tagedit clojure-mode-extra-font-locking clj-refactor cider yasnippet-snippets yasnippet multiple-cursors rust-mode scala-mode yaml-mode slime-company slime company-lsp lsp-ui lsp-mode markdown-mode magit company rainbow-delimiters zoom-window ag projectile smex ido-completing-read+ ido-vertical-mode flx-ido flx exec-path-from-shell use-package)))
+ '(safe-local-variable-values '((encoding . utf-8))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
