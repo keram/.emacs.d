ea83b67f3173b3fe42cfcbf9f31b7ececa634b45
Marek L <nospam.keram@gmail.com>
2018-09-15 20:45:27 +0100

    Comment capture templates for problem with C-c C-c

--- customizations/setup-org-capture.el
+++ customizations/setup-org-capture.el
@@ -2,20 +2,21 @@
 (setq org-default-notes-file (concat org-directory "/" "refile.org"))
 (setq org-log-done t)
 
+;; Does not work properly with emacs-snapshot ?!
 ;; Capture templates for: TODO tasks, Notes, habits, meetings, and org-protocol
-(setq org-capture-templates
-      (quote (("t" "todo" entry (file org-default-notes-file)
-               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("n" "note" entry (file org-default-notes-file)
-               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree (concat org-directory "/" "diary.org"))
-               "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file org-default-notes-file)
-               "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("m" "Meeting" entry (file org-default-notes-file)
-               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file org-default-notes-file)
-               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+;; (setq org-capture-templates
+;;       (quote (("t" "todo" entry (file org-default-notes-file)
+;;                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+;;               ("n" "note" entry (file org-default-notes-file)
+;;                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+;;               ("j" "Journal" entry (file+datetree (concat org-directory "/" "diary.org"))
+;;                "* %?\n%U\n" :clock-in t :clock-resume t)
+;;               ("w" "org-protocol" entry (file org-default-notes-file)
+;;                "* TODO Review %c\n%U\n" :immediate-finish t)
+;;               ("m" "Meeting" entry (file org-default-notes-file)
+;;                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+;;               ("h" "Habit" entry (file org-default-notes-file)
+;;                "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
 ;; Remove empty LOGBOOK drawers on clock out
 (defun bh/remove-empty-drawer-on-clock-out ()
