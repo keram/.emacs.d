3224b342b0c6fc1744c0311648871c2bfd723fee
Marek L <nospam.keram@gmail.com>
2018-11-04 18:02:52 +0100

    Add resonml / ocaml setup

--- init.el
+++ init.el
@@ -60,3 +60,6 @@
 
 ;; Hard-to-categorize customizations
 (load "misc.el")
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
