becaab0fac7aeefb601b4885c87d9a9f29198b8a
Marek L <nospam.keram@gmail.com>
2019-05-26 14:50:47 +0100

    sync latest system changes on win

--- system-custom.el
+++ system-custom.el
@@ -3,7 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(explicit-shell-file-name "~/.emacs_bash"))
+ '(custom-safe-themes
+   (quote
+    ("1d2f406a342499f0098f9388b87d05ec9b28ccb12ca548f4f5fa80ae368235b6" "54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(merlin-completion-with-doc t t)
+ '(package-selected-packages
+   (quote
+    (monokai-pro-theme zenburn-theme zoom-window yasnippet-snippets yaml-mode which-key web-mode utop use-package tuareg tagedit spacemacs-theme smex slime-company rust-mode reason-mode rbenv rainbow-delimiters projectile ox-reveal org-present org-plus-contrib org-bullets ocp-indent magit lsp-ui json-mode js2-mode js-doc ido-vertical-mode ido-completing-read+ go-mode gnuplot flx-ido exec-path-from-shell ensime company-lsp color-theme-sanityinc-tomorrow clojure-mode-extra-font-locking clj-refactor ag)))
+ '(utop-edit-command nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
