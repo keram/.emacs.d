1fb7256107e6f1b4804b6b2ff75f6257ce64cab6
Marek L <nospam.keram@gmail.com>
2019-05-27 19:51:39 +0100

    change to C-x C-M-g

--- customizations/editing.el
+++ customizations/editing.el
@@ -97,7 +97,7 @@
   (remove-hook 'server-switch-hook 'magit-commit-diff)
   (setq magit-refresh-status-buffer nil)
   (setq vc-handled-backends nil)
-  :bind (("C-x C-g" . magit-status))
+  :bind (("C-x C-m" . magit-status))
   )
 
 (use-package markdown-mode
