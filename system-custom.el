0e553a4810b9adada85c65599b959c447efde011
Marek L <nospam.keram@gmail.com>
2019-10-27 06:01:19 +0000

    updae system-custom.el

--- system-custom.el
+++ system-custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages
    (quote
-    (sr-speedbar zoom-window zenburn-theme yasnippet-snippets yaml-mode xterm-color which-key websocket web-mode utop use-package tuareg tagedit spacemacs-theme smex slime-company rust-mode request reason-mode rbenv rainbow-delimiters projectile ox-reveal org-present org-plus-contrib org-bullets ocp-indent oauth2 monokai-pro-theme magit lsp-ui key-chord json-mode js2-mode js-doc ido-vertical-mode ido-completing-read+ helm go-mode gnuplot git-messenger flx-ido exec-path-from-shell ensime emojify copy-as-format company-lsp color-theme-sanityinc-tomorrow clojure-mode-extra-font-locking clj-refactor circe all-the-icons alert ag))))
+    (define-word zoom-window zenburn-theme yasnippet-snippets yaml-mode xterm-color which-key websocket web-mode utop use-package tuareg tagedit sr-speedbar spacemacs-theme smex slime-company rust-mode rubocop reason-mode rbenv rainbow-delimiters projectile powerthesaurus poly-markdown ox-reveal org-web-tools org-tree-slide org-super-agenda org-ref org-presie org-present org-plus-contrib org-bullets ocp-indent oauth2 monokai-pro-theme magit lsp-ui logview json-mode js2-mode js-doc ipretty ido-vertical-mode ido-completing-read+ go-mode gnuplot git-messenger flx-ido exec-path-from-shell epresent ensime emojify doom-modeline csv-mode copy-as-format config-general-mode company-lsp color-theme-sanityinc-tomorrow clojure-mode-extra-font-locking clj-refactor circe browse-kill-ring apib-mode alert ag)))
+ '(zoom-window-mode-line-color "Black"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
