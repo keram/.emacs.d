fbb4e6d1ca981e0a431f61aa196750c47c2939b1
Marek <nospam.keram@gmail.com>
2020-05-08 18:29:16 +0100

    some setup update

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -5,9 +5,6 @@
    (exec-path-from-shell-copy-envs
     '("PATH")))
 
-;; (use-package exec-path-from-shell
-;;   :ensure t
-;;   )
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
   :demand t
@@ -17,3 +14,8 @@
 ;; shell scripts
 (setq-default sh-basic-offset 2)
 (setq-default sh-indentation 2)
+
+;; The addition of "-i" forces the bash shell into interactive mode,
+;; which leads to the sourcing of ~/.bashrc.
+;; https://stackoverflow.com/a/42036157
+;; (setq shell-command-switch "-ic")
--- system-custom.el
+++ system-custom.el
@@ -4,9 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc")
- '(package-selected-packages
-   (quote
-    (geiser biblio-core biblio zoom-window yasnippet-snippets yaml-mode xterm-color which-key web-mode use-package tuareg tagedit sr-speedbar smex slime-company scala-mode rust-mode rubocop reason-mode rbenv rainbow-delimiters projectile powerthesaurus poly-markdown ox-tufte ox-reveal org-web-tools org-ref org-present org-plus-contrib org-bullets ob-restclient magit lsp-ui logview json-mode js2-mode js-doc ipretty idris-mode ido-vertical-mode ido-completing-read+ haskell-mode go-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-messenger git-gutter-fringe+ flx-ido fill-column-indicator ess elixir-mode dumb-jump config-general-mode company-lsp clojure-mode-extra-font-locking clj-refactor browse-kill-ring apib-mode ag))))
+ '(org-attach-use-inheritance t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
