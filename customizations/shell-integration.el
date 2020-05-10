175568614e231d1f3e55db4af533dbef1a8399a5
Marek L <nospam.keram@gmail.com>
2020-05-10 11:06:58 +0100

    Simplify shell-integration.el

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -1,15 +1,12 @@
 ;; Sets up exec-path-from shell
 ;; https://github.com/purcell/exec-path-from-shell
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize)
-   (exec-path-from-shell-copy-envs
-    '("PATH")))
-
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
   :demand t
   :config
-  (progn (exec-path-from-shell-initialize)))
+  (progn
+    (exec-path-from-shell-initialize)
+    (exec-path-from-shell-copy-envs '("PATH"))))
 
 ;; shell scripts
 (setq-default sh-basic-offset 2)
