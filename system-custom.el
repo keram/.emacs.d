a9e9924f3bf2145f8b043f08d81472cd104eed5b
Marek L <nospam.keram@gmail.com>
2020-10-24 15:55:59 +0100

    sys-el

--- system-custom.el
+++ system-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages
-   '(lsp-mode transient helm-core helm zoom-window yasnippet-snippets yaml-mode xterm-color which-key web-mode use-package tuareg tide tagedit sqlup-mode sql-indent smex slime-company scala-mode rust-mode rubocop reason-mode rbenv rainbow-delimiters projectile posframe poly-markdown ox-tufte ox-reveal ox-gfm org-web-tools org-ref org-present org-plus-contrib org-bullets ob-restclient lsp-ui julia-mode json-mode js2-mode js-doc idris-mode ido-vertical-mode ido-completing-read+ haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-messenger git-gutter-fringe+ geiser flx-ido fill-column-indicator exec-path-from-shell ess elixir-mode dumb-jump company-lsp clojure-mode-extra-font-locking clj-refactor browse-kill-ring apib-mode ag))
+   '(flycheck zoom-window yasnippet-snippets yaml-mode xterm-color which-key use-package tuareg tide sqlup-mode sql-indent smex slime-company rubocop reason-mode rbenv rainbow-delimiters projectile poly-markdown ox-tufte ox-reveal ox-gfm org-web-tools org-ref org-present org-plus-contrib org-bullets ob-restclient json-mode js2-mode ipretty idris-mode ido-vertical-mode ido-completing-read+ haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-gutter-fringe+ geiser flx-ido fill-column-indicator exec-path-from-shell ess elixir-mode dumb-jump clojure-mode-extra-font-locking clj-refactor browse-kill-ring apib-mode ag))
  '(zoom-window-mode-line-color "Black"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
