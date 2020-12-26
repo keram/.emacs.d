b3cab22b5914562acddfa450ec2c1a29057297b0
Marek L <nospam.keram@gmail.com>
2020-12-26 11:27:13 +0000

    Remove commented out packages

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,13 +1,3 @@
-;; javascript / html
-;; (use-package tagedit
-;;   :defer t
-;;   :ensure t)
-
-;; (use-package json-mode
-;;   :mode "\\.json\\'"
-;;   :defer t
-;;   :ensure t)
-
 (use-package js2-mode
   :defer t
   :ensure t
@@ -18,32 +8,6 @@
 ;; (use-package js-doc
 ;;   :defer t
 ;;   :ensure t)
-
-;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-;; (add-hook 'html-mode-hook 'subword-mode)
-;; (setq js-indent-level 4)
-;; (eval-after-load "sgml-mode"
-;;   '(progn
-;;      (require 'tagedit)
-;;      (tagedit-add-paredit-like-keybindings)
-;;      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
-
-;; (use-package web-mode
-;;   :defer t
-;;   :ensure t
-;;   :mode ("\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'" "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'")
-;; ;;   :config
-;; ;;   (add-hook 'web-mode-hook  'my-web-mode-hook)
-;; )
-
-;; (defun test-javascript ()
-;;   (concat "npm run test --prefix " (file-at-git-root "")))
-
-;; (add-hook ;; 'js2-mode
-;;           -hook
-;;           (lambda ()
-;;             (set (make-local-variable 'testing-command)
-;;                  (test-javascript))))
 
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
