2a8c5ea2002dbee19951a4f5dfcc20b63834e49e
Marek L <nospam.keram@gmail.com>
2020-12-27 17:35:11 +0000

    update sys custom.el

--- system-custom.el
+++ system-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages
-   '(zoom-window yasnippet-snippets xterm-color which-key use-package tide sqlup-mode sql-indent smex slime-company rubocop rbenv rainbow-delimiters projectile poly-markdown paredit ox-tufte ox-reveal ox-gfm org-web-tools org-present org-bullets ob-restclient multiple-cursors lsp-ui js2-mode ipretty idris-mode ido-vertical-mode ido-completing-read+ haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-gutter-fringe+ geiser flx-ido fill-column-indicator extmap exec-path-from-shell ess elixir-mode editorconfig dumb-jump clojure-mode-extra-font-locking cider browse-kill-ring apib-mode ag)))
+   '(lsp-mode esxml org-ref zoom-window yasnippet-snippets xterm-color which-key use-package tide sqlup-mode sql-indent smex slime-company rubocop rbenv rainbow-delimiters projectile poly-markdown pdf-tools paredit ox-tufte ox-reveal ox-gfm org-web-tools org-present org-bullets ob-restclient multiple-cursors lsp-ui key-chord js2-mode ivy ipretty idris-mode ido-vertical-mode ido-completing-read+ hydra htmlize helm-bibtex haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-gutter-fringe+ geiser flx-ido fill-column-indicator extmap exec-path-from-shell ess elixir-mode editorconfig dumb-jump clojure-mode-extra-font-locking cider browse-kill-ring apib-mode ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
