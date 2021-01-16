556444c018695674571e448c79aff8b82fc5b1fc
Marek Labos <marek@syftapp.com>
2021-01-16 21:52:56 +0000

    add mac system-custom.el

---
+++ darwin_MAREKL-MBP-local_system-custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-command "/usr/bin/pandoc")
+ '(package-selected-packages
+   '(org-jira zoom-window yasnippet-snippets xterm-color which-key use-package tide sqlup-mode sql-indent smex slime-company rubocop rbenv rainbow-delimiters projectile poly-markdown paredit ox-tufte ox-reveal ox-jira ox-gfm org-web-tools org-ref org-present org-bullets ob-restclient multiple-cursors json-snatcher json-reformat js2-mode ipretty inflections idris-mode ido-vertical-mode ido-completing-read+ haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-gutter-fringe+ geiser flx-ido fill-column-indicator exec-path-from-shell ess elixir-mode editorconfig dumb-jump clojure-mode-extra-font-locking cider caml browse-kill-ring apib-mode ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
