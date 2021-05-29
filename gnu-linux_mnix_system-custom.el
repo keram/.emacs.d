76ccb72d3a02ded7c0879bfc74bdaf76545cf079
Marek L <nospam.keram@gmail.com>
2021-05-29 16:58:52 +0100

    Update mnix deps

--- gnu-linux_mnix_system-custom.el
+++ gnu-linux_mnix_system-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages
-   '(zoom-window yasnippet-snippets xterm-color xml+ with-editor which-key use-package transient tide sqlup-mode sql-indent slime-company rubocop rbenv rainbow-delimiters projectile poly-markdown pdf-tools paredit ox-tufte ox-reveal ox-gfm org-web-tools org-present org-bullets ob-restclient multiple-cursors memoize lsp-ui key-chord js2-mode ivy ipretty idris-mode hydra htmlize helm-bibtex haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode geiser-racket geiser-chez flx fill-column-indicator extmap exec-path-from-shell ess elixir-mode editorconfig dumb-jump dash-functional clojure-mode-extra-font-locking cider browse-kill-ring apib-mode ag)))
+   '(gitattributes-mode gitconfig-mode gitignore-mode projectile zoom-window yasnippet-snippets xterm-color xml+ with-editor which-key use-package transient tide sqlup-mode sql-indent slime-company rubocop rbenv rainbow-delimiters poly-markdown paredit ox-tufte ox-reveal ox-gfm org-web-tools org-present org-bullets ob-restclient multiple-cursors memoize lsp-ui key-chord js2-mode ivy ipretty idris-mode hydra htmlize ht helm-bibtex haskell-mode gnuplot geiser-racket geiser-chez flx fill-column-indicator extmap exec-path-from-shell ess elixir-mode editorconfig dumb-jump dash-functional clojure-mode-extra-font-locking cider browse-kill-ring async apib-mode ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
