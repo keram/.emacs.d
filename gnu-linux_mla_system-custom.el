b942d159ba3748549f705c4bc56645cca3fa77d6
Marek L <nospam.keram@gmail.com>
2021-05-29 12:49:46 +0100

    Cleanup

--- gnu-linux_mla_system-custom.el
+++ gnu-linux_mla_system-custom.el
@@ -6,7 +6,14 @@
  '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages
    (quote
-    (slime parsebib ivy helm git-commit dash company zoom-window yasnippet-snippets yaml-mode xterm-color which-key use-package tide sqlup-mode sql-indent smex slime-company rubocop rbenv rainbow-delimiters projectile poly-markdown ox-tufte ox-reveal ox-gfm org-web-tools org-ref org-present org-pdftools org-bullets ob-restclient js2-mode ipretty idris-mode ido-vertical-mode ido-completing-read+ haskell-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-gutter-fringe+ geiser flx-ido fill-column-indicator exec-path-from-shell ess elixir-mode editorconfig dumb-jump clojure-mode-extra-font-locking clj-refactor browse-kill-ring apib-mode ag))))
+    (slime parsebib dash company zoom-window yasnippet-snippets xterm-color
+           which-key use-package tide sqlup-mode sql-indent smex slime-company
+           rubocop rbenv rainbow-delimiters projectile poly-markdown
+           ox-tufte ox-reveal ox-gfm org-web-tools org-ref org-present org-pdftools org-bullets ob-restclient
+           js2-mode ipretty idris-mode haskell-mode gnuplot gitignore-mode
+           gitconfig-mode gitattributes-mode geiser fill-column-indicator
+           exec-path-from-shell ess elixir-mode editorconfig dumb-jump
+           clojure-mode-extra-font-locking browse-kill-ring apib-mode ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
